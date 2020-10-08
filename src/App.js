e6f2a67fb73758ca23bd8bb522ebce67c2175fa9
Evangelos Zisimatos <evangelos.zisimatos@gmail.com>
2020-10-08 15:35:10 +0300

    App updated with a shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -3,37 +3,14 @@ import { Route } from 'react-router-dom';
 
 import './App.css';
 
-import HomePage from "./pages/homepage/homepage.component";
-
-const HatsPage = () => {
-    return (<div><h2>This is Hats' page!</h2></div>);
-}
-
-const JacketsPage = () => {
-    return (<div><h2>This is Jackets' page!</h2></div>);
-}
-
-const SneakersPage = () => {
-    return (<div><h2>This is Sneakers' page!</h2></div>);
-}
-
-const WomensPage = () => {
-    return (<div><h2>This is Women's page!</h2></div>);
-}
-
-const MensPage = () => {
-    return (<div><h2>This is Men's page!</h2></div>);
-}
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
       <div>
           <Route exact path='/' component={HomePage} />
-          <Route exact path='/shop/hats' component={HatsPage} />
-          <Route exact path='/shop/jackets' component={JacketsPage} />
-          <Route exact path='/shop/sneakers' component={SneakersPage} />
-          <Route exact path='/shop/womens' component={WomensPage} />
-          <Route exact path='/shop/mens' component={MensPage} />
+          <Route exact path='/shop' component={ShopPage} />
       </div>
   );
 }

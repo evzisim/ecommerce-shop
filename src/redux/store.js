5953bb8bb9e8ffc66948e7232ce9876584b17cd1
Evangelos Zisimatos <evangelos.zisimatos@gmail.com>
2020-10-15 09:27:24 +0300

    cardIcon and cartDropdown components added to the application, cardReducer implemented as well

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,7 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
-
-const composeEnhancers = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE__|| compose;
 
 const middlewares = [logger];
 
